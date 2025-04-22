1a28799b80b7e9343cf33aa57570622402a47199
Sheelgupte <sheelgupte@gmail.com>
2025-04-22 17:41:48 -0500

    Update useDivorceRates.ts

--- src/hooks/useDivorceRates.ts
+++ src/hooks/useDivorceRates.ts
@@ -12,7 +12,7 @@ export const useDivorceRates = (selectedState: string) => {
   const fetchDivorceRates = async (): Promise<DivorceRateChartData[]> => {
     const { data, error } = await supabase
       .from("divorce_rate")
-      .select(`Year, State, "divorce_rate"`);
+      .select(Year, State, "divorce_rate");
 
     if (error || !data) {
       console.error("Error loading divorce rates:", error);
